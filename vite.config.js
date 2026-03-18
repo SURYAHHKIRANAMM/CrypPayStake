abe8352b43ec6e1f5122437ef2005a5eb2d72a00
CrypPayStake Dev <gauravkumarvrma@gmail.com>
2026-03-18 22:53:45 +0530

    Fix mobile crash with renderChunk patch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "fix-rainbowkit-mobile",
+      enforce: "post",
+      renderChunk(code) {
+        if (code.includes("toLowerCase")) {
+          return code.replace(
+            /(\w+)\.toLowerCase\(\)/g,
+            '(($1)?.toLowerCase?.()??"")'
+          );
+        }
+        return code;
+      },
+    },
+  ],
+});
