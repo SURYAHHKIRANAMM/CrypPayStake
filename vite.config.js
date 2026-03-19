b6e07cdf3195aedce01c9afa6460f441ebceadfa
CrypPayStake Dev <gauravkumarvrma@gmail.com>
2026-03-19 13:32:47 +0530

    Stable working dashboard and wallet flow

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "fix-rainbowkit-mobile",
-      enforce: "post",
-      renderChunk(code) {
-        if (code.includes("toLowerCase")) {
-          return code.replace(
-            /(\w+)\.toLowerCase\(\)/g,
-            '(($1)?.toLowerCase?.() ?? "")'
-          );
-        }
-        return code;
-      },
-    },
-  ],
-
+  plugins: [react()],
   define: {
     global: "globalThis",
     "process.env": {},
